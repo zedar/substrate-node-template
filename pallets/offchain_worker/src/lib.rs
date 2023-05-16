77e32a3a581485d59bfa8f7ee2dbf72ec4338994
zedar <zakrzewski.robert@gmail.com>
2023-05-16 23:27:33 +0200

    Add pallet skeleton

--- pallets/offchain_worker/src/lib.rs
+++ pallets/offchain_worker/src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-	left + right
-}
+#![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(test)]
-mod tests {
-	use super::*;
+pub use pallet::*;
 
-	#[test]
-	fn it_works() {
-		let result = add(2, 2);
-		assert_eq!(result, 4);
+// dev_mode relax some restrictions placed on production pallets, such as no need to specify a weight on every `#[pallet::call]`
+// Note: remove dev_mode before deploying in a production runtime.
+#[frame_support::pallet(dev_mode)]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	// Defines generic data types that the pallet uses
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+
 	}
 }
