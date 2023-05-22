0fab06266fba6b6676b9988bcb80724e7c11bdf3
zedar <zakrzewski.robert@gmail.com>
2023-05-22 08:19:29 +0200

    Add mint/create/register a new feed

--- pallets/offchain_worker/src/lib.rs
+++ pallets/offchain_worker/src/lib.rs
@@ -2,12 +2,14 @@
 
 pub use pallet::*;
 
-// dev_mode relax some restrictions placed on production pallets, such as no need to specify a weight on every `#[pallet::call]`
-// Note: remove dev_mode before deploying in a production runtime.
+// dev_mode relax some restrictions placed on production pallets, such as no need to specify a
+// weight on every `#[pallet::call]` Note: remove dev_mode before deploying in a production runtime.
 #[frame_support::pallet(dev_mode)]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
-	use frame_support::traits::{Currency, Randomness};
+	use frame_support::{
+		pallet_prelude::*,
+		traits::{Currency, Randomness},
+	};
 	use frame_system::pallet_prelude::*;
 
 	#[pallet::pallet]
@@ -16,12 +18,14 @@ pub mod pallet {
 
 	// Defines generic data types that the pallet uses
 	// By including the Currency and Randomness interfaces the pallet will be able to:
-	//	- access and manipulate user accounts and balances
-	//	- generate on-chain randomness
+	// 	- access and manipulate user accounts and balances
+	// 	- generate on-chain randomness
 	// 	- set the limit on the number of newsletters a single user can subscribe to
 	// frame_system|frame_support types: AccountId, BlockNumber, Hash
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
 		type Currency: Currency<Self::AccountId>;
 
 		// mapping of a feed hash to the block number
@@ -52,8 +56,9 @@ pub mod pallet {
 
 	type FeedId = [u8; 16];
 
-	// TypeInfo macro forces to parse the underlying object into some JSON type. The Feed type is generic over T,
-	// we don't want to include it in the TypeInfo generation step. That's why skip_type_params(T) is needed
+	// TypeInfo macro forces to parse the underlying object into some JSON type. The Feed type is
+	// generic over T, we don't want to include it in the TypeInfo generation step. That's why
+	// skip_type_params(T) is needed
 	#[derive(Clone, Encode, Decode, PartialEq, TypeInfo, RuntimeDebug, MaxEncodedLen)]
 	#[scale_info(skip_type_params(T))]
 	pub struct Feed<T: Config> {
@@ -77,7 +82,7 @@ pub mod pallet {
 	// OptionQuery - None is returned,
 	// ResultQuery - Err is returned.
 	#[pallet::storage]
-	pub(super) type FeedCount<T: Config> = StorageValue<_, u64, ValueQuery>;
+	pub(super) type FeedCount<T: Config> = StorageValue<_, u32, ValueQuery>;
 
 	// Stores a map of feeds (each with unique_id) to their properties
 	// The Twox64Concat is hashing algorithm used to store the map value.
@@ -105,5 +110,67 @@ pub mod pallet {
 		DuplicatedSubscription,
 		// an account can't exceed `Config::MaxSubscriptions`
 		MaxSubscriptionsExceeded,
+	}
+
+	// generate_deposit generates a helper function on Pallet that handles event depositing/sending
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		// runtime sends FeedCreated event when new feed has been successfully created
+		FeedCreated { feed: FeedId, owner: T::AccountId },
+	}
+
+	// Pallet internal functions
+	impl<T: Config> Pallet<T> {
+		// generate unique id for a new feed
+		fn new_unique_id() -> FeedId {
+			// create randomness
+			let random = T::FeedRandomness::random(&b"unique_id"[..]).0;
+			// randomness payload enables that multiple feeds can be created in the same block
+			let unique_payload = (
+				random,
+				frame_system::Pallet::<T>::extrinsic_index().unwrap_or_default(),
+				frame_system::Pallet::<T>::block_number(),
+			);
+			let encoded_payload = unique_payload.encode();
+			frame_support::Hashable::blake2_128(&encoded_payload)
+		}
+
+		// Mint/create new feed
+		pub fn mint_feed(
+			owner: &T::AccountId,
+			unique_id: FeedId,
+			payment: PaymentType,
+			url: &[u8],
+		) -> Result<FeedId, DispatchError> {
+			// create new feed
+			let feed = Feed::<T> {
+				unique_id,
+				owner: owner.clone(),
+				fee: None,
+				payment_type: payment,
+				registration_url: BoundedVec::<u8, T::MaxRegistrationUrlLength>::try_from(
+					url.to_vec(),
+				)
+				.map_err(|_| Error::<T>::RegistrationUrlTooLong)?,
+			};
+
+			// check that feed does not exist in the storage map
+			ensure!(!Feeds::<T>::contains_key(&feed.unique_id), Error::<T>::DuplicatedFeed);
+
+			// check that a new feed does not exceed max available feeds
+			let count = FeedCount::<T>::get();
+			let new_count = count.checked_add(1).ok_or(Error::<T>::FeedsOverflow)?;
+			ensure!(new_count <= T::MaxFeeds::get(), Error::<T>::FeedsOverflow);
+
+			// write new feed to the storage and update the count
+			Feeds::<T>::insert(feed.unique_id, feed);
+			FeedCount::<T>::put(new_count);
+
+			// deposit the event
+			Self::deposit_event(Event::<T>::FeedCreated { feed: unique_id, owner: owner.clone() });
+
+			Ok(unique_id)
+		}
 	}
 }
